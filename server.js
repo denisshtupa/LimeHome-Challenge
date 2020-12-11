169232b09821b99cf458d280a740390aa6ebe8c2
denis.shtupa <denis.shtupa@3i-solutions.net>
2020-12-11 23:49:09 +0100

    xx

--- server.js
+++ server.js
@@ -1,3 +1,8 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
 app.use(express.static('./dist/lime-home-challenge'));
 
 app.get('/*', function(req, res) {
